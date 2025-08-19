e851429003f86fbdb14b428552ef5646eab5faf0
Meena Ale Magar <meenaalemagar@Mac.lan>
2025-08-18 21:35:35 -0500

    Added user in BookingDto

--- src/main/java/com/airbnb/backendAirbnb/dto/BookingDto.java
+++ src/main/java/com/airbnb/backendAirbnb/dto/BookingDto.java
@@ -7,9 +7,15 @@ import java.time.LocalDate;
 public class BookingDto {
 
 
+
     @NotNull(message = "Start date must be provided")
     private LocalDate startDate;
 
     @NotNull(message = "End date must be provided")
     private LocalDate endDate;
+
+    private String Day;
+     private String User;
+
+
 }
