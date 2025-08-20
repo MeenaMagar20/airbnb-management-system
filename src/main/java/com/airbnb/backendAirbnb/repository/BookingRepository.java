3d468dac6adc93301337e7ca956067fa3a7c1f38
Meena Ale Magar <meenaalemagar@Mac.lan>
2025-08-19 23:40:02 -0500

    Added end points in BookingController (Main)

--- src/main/java/com/airbnb/backendAirbnb/repository/BookingRepository.java
+++ src/main/java/com/airbnb/backendAirbnb/repository/BookingRepository.java
@@ -1,6 +1,9 @@
 package com.airbnb.backendAirbnb.repository;
 
-public class BookingRepository {
+import com.airbnb.backendAirbnb.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
